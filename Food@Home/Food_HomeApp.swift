511273b440a0a001e25a553aa513b42fb78f300f
Derek Howes <d.howes24@gmail.com>
2023-06-06 15:54:25 -0400

    Initial Commit

---
+++ Food@Home/Food_HomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Food_HomeApp.swift
+//  Food@Home
+//
+//  Created by Derek Howes on 6/6/23.
+//
+
+import SwiftUI
+
+@main
+struct Food_HomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
