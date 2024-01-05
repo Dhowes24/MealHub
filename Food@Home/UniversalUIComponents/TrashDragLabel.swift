a1e89bd03ce87ca71bbba14a474c572c600ee784
Derek Howes <d.howes24@gmail.com>
2024-01-04 22:23:49 -0500

    Time Selector Changes: * Radio buttons got changed to check boxes * Time Selector options but in proper chrono order * Added selector delete drag gesture with custom view modifier * Button adds more timeSelectorObjects to array and view * TrashCan custom label

---
+++ Food@Home/UniversalUIComponents/TrashDragLabel.swift
@@ -0,0 +1,18 @@
+//
+//  TrashDragLabel.swift
+//  Food@Home
+//
+//  Created by Derek Howes on 12/21/23.
+//
+
+import SwiftUI
+
+struct TrashDragLabel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TrashDragLabel()
+}
