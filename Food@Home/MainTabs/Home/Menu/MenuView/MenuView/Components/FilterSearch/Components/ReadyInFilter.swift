08ed863d8b8f07ad942262bbe29fbd30462e62ad
Derek Howes <D.howes24@gmail.com>
2024-02-07 22:14:40 -0500

    ReadyInFormatting: * Downloaded WrappingHStack package for an Hstack that will create newlines after items exceed with of page

--- Food@Home/MainTabs/Home/Menu/MenuView/MenuView/Components/FilterSearch/Components/ReadyInFilter.swift
+++ Food@Home/MainTabs/Home/Menu/MenuView/MenuView/Components/FilterSearch/Components/ReadyInFilter.swift
@@ -4,71 +4,71 @@
 //
 //  Created by Derek Howes on 1/18/24.
 //
-import WaterfallGrid
 import SwiftUI
+import WrappingHStack
 
 struct ReadyInFilter: View {
     @Binding var dict: [String: Bool]
     @Environment(\.dismiss) private var dismiss
     
     var body: some View {
+        subViewHeader(headerText: "Pick time")
         
-        GeometryReader { geo in
-            VStack(alignment: .leading) {
-                
-                subViewHeader(headerText: "Pick time")
-
-                Text("Ready in less than")
-                    .font(.customSystem(size: 14, weight: .bold))
-                    .padding(.bottom, 24)
-                
-                let enumeratedItems = Array(dict.sorted(by: { Int($0.key) ?? 1 < Int($1.key) ?? 1 })).enumerated()
-                let arrayEnumeratedItems = Array(enumeratedItems)
-                
-                ForEach(arrayEnumeratedItems, id: \.element.key)  { (index, element) in
-                    Group {
-                        if element.value == true {
-                            HStack {
-                                Text("\(element.key.description) minutes")
-                                    .font(.customSystem(size: 14, weight: .bold))
-                                    .foregroundStyle(offWhite)
-                                    .padding(.horizontal, 16)
-                                    .padding(.vertical, 13)
-                                    .background(
-                                        Capsule()
-                                    )
-                            }
-                        } else {
-                            HStack {
-                                Text("\(element.key.description) minutes")
-                                    .font(.customSystem(size: 14, weight: .bold))
-                                    .padding(.horizontal, 16)
-                                    .padding(.vertical, 13)
-                                
-                                    .overlay(
-                                        Capsule()
-                                            .stroke(customGrey, lineWidth: 1)
-                                    )
-                            }
+        VStack(alignment: .leading) {
+            Text("Ready in less than")
+                .font(.customSystem(size: 14, weight: .bold))
+                .padding(.vertical, 24)
+            
+            let enumeratedItems = Array(dict.sorted(by: { Int($0.key) ?? 1 < Int($1.key) ?? 1 })).enumerated()
+            let arrayEnumeratedItems = Array(enumeratedItems)
+            
+            WrappingHStack(arrayEnumeratedItems, id: \.self, spacing: .constant(22), lineSpacing: 16.0) { (index, element) in
+                Group {
+                    if element.value == true {
+                        HStack {
+                            Text("\(element.key.description) minutes")
+                                .font(.customSystem(size: 14, weight: .bold))
+                                .foregroundStyle(offWhite)
+                                .padding(.horizontal, 16)
+                                .padding(.vertical, 13)
+                                .background(
+                                    Capsule()
+                                )
+                        }
+                    } else {
+                        HStack {
+                            Text("\(element.key.description) minutes")
+                                .font(.customSystem(size: 14, weight: .bold))
+                                .padding(.horizontal, 16)
+                                .padding(.vertical, 13)
+                            
+                                .overlay(
+                                    Capsule()
+                                        .stroke(customGrey, lineWidth: 1)
+                                )
                         }
                     }
-                    .onTapGesture {
-                        dict.keys.forEach { key in
-                            dict[key] = false
-                        }
-                        dict[element.key] = !element.value
-                        
-                        encodeUserDefaults(filterDict: dict, keyString: "Ready In")
+                }
+                .onTapGesture {
+                    dict.keys.forEach { key in
+                        dict[key] = false
                     }
+                    dict[element.key] = !element.value
+                    
+                    encodeUserDefaults(filterDict: dict, keyString: "Ready In")
                 }
             }
-            .onAppear(
-                perform: {
-                    self.dict = decodeUserDefaults("Ready In")
-                }
-            )
-            .navigationBarBackButtonHidden(true)
+            
+            Spacer()
+            
         }
+        .padding(.horizontal, 16)
+        .onAppear(
+            perform: {
+                self.dict = decodeUserDefaults("Ready In")
+            }
+        )
+        .navigationBarBackButtonHidden(true)
     }
 }
 
@@ -77,7 +77,6 @@ struct ReadyInFilter: View {
         @State var bindingDict: [String: Bool] = ["15": false, "30": false, "45": false, "60": false, "120": false, "180": true]
         var body: some View {
             ReadyInFilter(dict: $bindingDict)
-
         }
     }
     return PreviewWrapper()
