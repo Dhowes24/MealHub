1824d1328656a12dd0b20a45b430098655b9dd50
Derek Howes <D.howes24@gmail.com>
2024-02-08 15:07:00 -0500

    Use live data RecipeDetails fetch request

--- MealHub/ReuseableViews/RecipeDetails/RecipeDetailsViewModel.swift
+++ MealHub/ReuseableViews/RecipeDetails/RecipeDetailsViewModel.swift
@@ -40,51 +40,51 @@ extension RecipeDetailsView {
         
         //Test Data Function
         //------------------------------------------------------------------------------------------
-        func fetchIndividualRecipe(id: Int) async throws {
-            do {
-                if let filePath = Bundle.main.path(forResource: "RecipeDetailsTestInfo", ofType: "json") {
-                    let fileUrl = URL(fileURLWithPath: filePath)
-                    let data = try Data(contentsOf: fileUrl)
-                    
-                    if let decodedResponse = try? JSONDecoder().decode(RecipeInfo.self, from: data) {
-                        recipeInfo = decodedResponse
-                    }
-                    else {
-                        print("Error between Data Model and JSON schema")
-                    }
-                }
-            } catch {
-                print("error: \(error)")
-            }
-        }
+//        func fetchIndividualRecipe(id: Int) async throws {
+//            do {
+//                if let filePath = Bundle.main.path(forResource: "RecipeDetailsTestInfo", ofType: "json") {
+//                    let fileUrl = URL(fileURLWithPath: filePath)
+//                    let data = try Data(contentsOf: fileUrl)
+//                    
+//                    if let decodedResponse = try? JSONDecoder().decode(RecipeInfo.self, from: data) {
+//                        recipeInfo = decodedResponse
+//                    }
+//                    else {
+//                        print("Error between Data Model and JSON schema")
+//                    }
+//                }
+//            } catch {
+//                print("error: \(error)")
+//            }
+//        }
         
         //Live Data Function
         //------------------------------------------------------------------------------------------
-        //        func fetchIndividualRecipe(id: Int) async throws {
-        //            let request = NSMutableURLRequest(url: NSURL(string: "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/\(id)/information")! as URL,
-        //                                              cachePolicy: .useProtocolCachePolicy,
-        //                                              timeoutInterval: 10.0)
-        //            request.httpMethod = "GET"
-        //            request.allHTTPHeaderFields = headers
-        //
-        //            let session = URLSession.shared
-        //            let dataTask = session.dataTask(with: request as URLRequest, completionHandler: { (data, response, error) -> Void in
-        //                if (error != nil) {
-        //                    print(error as Any)
-        //                } else {
-        //                    if let decodedResponse = try? JSONDecoder().decode(RecipeInfo.self, from: data!) {
-        //                        Task {
-        //                            await MainActor.run {
-        //                                self.recipeInfo = decodedResponse
-        //                            }
-        //                        }
-        //                    } else {
-        //                        print(String(describing: error))
-        //                    }
-        //                }
-        //            })
-        //            dataTask.resume()
-        //        }
+                func fetchIndividualRecipe(id: Int) async throws {
+                    let request = NSMutableURLRequest(url: NSURL(string: "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/\(id)/information")! as URL,
+                                                      cachePolicy: .useProtocolCachePolicy,
+                                                      timeoutInterval: 10.0)
+                    request.httpMethod = "GET"
+                    request.allHTTPHeaderFields = headers
+        
+                    let session = URLSession.shared
+                    let dataTask = session.dataTask(with: request as URLRequest, completionHandler: { (data, response, error) -> Void in
+                        if (error != nil) {
+                            print(error as Any)
+                        } else {
+                            if let decodedResponse = try? JSONDecoder().decode(RecipeInfo.self, from: data!) {
+                                Task {
+                                    await MainActor.run {
+                                        self.recipeInfo = decodedResponse
+                                    }
+                                }
+                            } else {
+                                print(String(describing: error))
+                            }
+                        }
+                    })
+                    dataTask.resume()
+                }
         
         func saveButtonTapped(savedRecipes: FetchedResults<SavedRecipes>, moc: NSManagedObjectContext) {
             if saved {
